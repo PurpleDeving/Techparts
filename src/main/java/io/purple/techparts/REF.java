68debb26f0cb046daf3e54f40cbbfd935d01ca41
Purple <lazzydeving@gmail.com>
2024-06-01 15:29:44 +0200

    Push for Greptile

---
+++ src/main/java/io/purple/techparts/REF.java
@@ -0,0 +1,6 @@
+package io.purple.techparts;
+
+public class REF {
+
+    public static final String ID = "techparts";
+}
---
+++ src/main/java/io/purple/techparts/hammer/ResourcePackAdapter.java
@@ -0,0 +1,66 @@
+package io.purple.techparts.hammer;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.MetadataSectionSerializer;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.server.packs.resources.IoSupplier;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraftforge.event.AddPackFindersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ResourcePackAdapter
+{
+    public static final List<PackResources> BUILTIN_PACKS = new ArrayList<>();
+
+    public static void registerResourcePack(PackResources pack)
+    {
+        if(!BUILTIN_PACKS.contains(pack))
+            BUILTIN_PACKS.add(pack);
+    }
+
+    @SubscribeEvent
+    public static void addPacks(AddPackFindersEvent e)
+    {
+        e.addRepositorySource((add) ->
+        {
+            for(PackResources pack : ResourcePackAdapter.BUILTIN_PACKS)
+            {
+                if(pack instanceof IRegisterListener rl)
+                    rl.onPreRegistered(new ResourceLocation(pack.packId()));
+
+                add.accept(Pack.create(
+                        pack.packId(),
+                        Component.literal(pack.packId()),
+                        true,
+                        (s) -> pack,
+                        new Pack.Info(
+                                Component.translatable("fml.resources.modresources", 1),
+                                SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES),
+                                FeatureFlagSet.of()
+                        ),
+                        PackType.CLIENT_RESOURCES,
+                        Pack.Position.TOP,
+                        pack.isHidden(),
+                        PackSource.BUILT_IN
+                ));
+
+                if(pack instanceof IRegisterListener rl)
+                    rl.onPostRegistered(new ResourceLocation(pack.packId()));
+            }
+        });
+    }
+}
